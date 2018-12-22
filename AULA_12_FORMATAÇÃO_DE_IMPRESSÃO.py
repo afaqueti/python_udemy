bdeda70728d6bade25fe6c2c846383e9030dee74
faqueti <afaqueti@gmail.com>
2018-12-22 10:44:40 -0200

    FORMATÇÃO DE IMPRESSÃO - AULA 12

---
+++ AULA_12_FORMATAÇÃO_DE_IMPRESSÃO.py
@@ -0,0 +1,12 @@
+# FORMATÇÃO DE IMPRESSÃO
+
+
+a1 = 'teste'
+a = 123
+print('Este novo [{}] para confirmar estrutura'.format(a))
+print('Este novo %s para conf estrututa: %2.2f' %(a,(13.1444)))
+
+
+# A linha abaixo foi gerada para definir o formato das casas decimais conforme valor.
+
+print('Novo teste para formatar núemro %5.3f' %(13.7777))
