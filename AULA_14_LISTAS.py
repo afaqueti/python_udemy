17330e6d64e03d4a9043c20eb7be250062393225
faqueti <afaqueti@gmail.com>
2018-12-22 23:22:05 -0200

    LISTA - CONTANDO QUANTOS CARACTERES EXISTE NA COLUNA STRING

--- AULA_14_LISTAS.py
+++ AULA_14_LISTAS.py
@@ -6,9 +6,15 @@ lista1 = [1,2,3,4,5]
 print('Lista Um --- ',lista1)
 
 # LISTA - CONTANDO QUANTOS CARACTERES EXISTE NA COLUNA STRING
-lista2 = [1,3,4,'Alessandro',10]
+lista2 = [1,3,4,'Nicolas',10]
 print('lLsta Dois --- ',lista2)
 print('Lista Dois',lista2[3:4])
 s = str(lista2[3:4])
-print(s[2:-2])
-print(len(s[2:-2]))
+print('Nome digitado como string --- ',s[2:-2])
+print('A string contem {} letras'.format(len(s[2:-2])))
+
+
+# JUNTANDO AS DUAS LISTA (LISTA1 + LISTA)
+
+a = lista1 + lista2
+print(a)
