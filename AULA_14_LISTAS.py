0a2e6d7dc6a33aa4288044fc2fc4a1c12b7a1f22
faqueti <afaqueti@gmail.com>
2018-12-22 23:07:51 -0200

    LISTA - CONTANDO QUANTOS CARACTERES EXISTE NA COLUNA STRING

---
+++ AULA_14_LISTAS.py
@@ -0,0 +1,14 @@
+# LISTAS
+
+# LISTA NO PYTHON NÃO TEM LIMITES, DIFERENTE DE OUTRAS LINGUAGENS QUE USA ESSE METODO COMO "ARRAY"
+
+lista1 = [1,2,3,4,5]
+print('Lista Um --- ',lista1)
+
+# LISTA - CONTANDO QUANTOS CARACTERES EXISTE NA COLUNA STRING
+lista2 = [1,3,4,'Alessandro',10]
+print('lLsta Dois --- ',lista2)
+print('Lista Dois',lista2[3:4])
+s = str(lista2[3:4])
+print(s[2:-2])
+print(len(s[2:-2]))
