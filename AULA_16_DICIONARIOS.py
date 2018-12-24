0a1dc9092d3ea566764d7adf1efc4830cbcc36d3
faqueti <afaqueti@gmail.com>
2018-12-24 15:37:22 -0200

    DICIONARIOS

---
+++ AULA_16_DICIONARIOS.py
@@ -0,0 +1,15 @@
+# DICIONÁRIOS
+# OS DICIONÁRIOS TRABALHA COM UMA CHAVE PARA INDEXAR O EXEMPLO DE UMA LISTA
+
+my_list = [1,2,3,4]
+print(my_list[0:4])
+
+my_dict = {'chave1':1.2, 'chave2':'Teste'}
+my_dict2 = {'chave1':1.2,'chave3':'novoteste'}
+
+my_dict3 = {'chave5':my_list}
+
+
+print(my_dict)
+print(my_dict2)
+print(my_dict3)
