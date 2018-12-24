5745acf3bde8c2a5dff4c1b7e5abd88420ec4fa4
faqueti <afaqueti@gmail.com>
2018-12-24 15:53:20 -0200

    DICIONARIOS

--- AULA_16_DICIONARIOS.py
+++ AULA_16_DICIONARIOS.py
@@ -1,15 +1,28 @@
 # DICIONÁRIOS
-# OS DICIONÁRIOS TRABALHA COM UMA CHAVE PARA INDEXAR O EXEMPLO DE UMA LISTA
+# OS DICIONÁRIOS TRABALHA COM UMA CHAVE PARA INDEXAR O EXEMPLO DE UMA LISTA.
+# DICIONARIOS SÃO MULTAVEIS PODE SER ALTERADOS
 
 my_list = [1,2,3,4]
 print(my_list[0:4])
 
 my_dict = {'chave1':1.2, 'chave2':'Teste'}
-my_dict2 = {'chave1':1.2,'chave3':'novoteste'}
+my_dict2 = {'chave4':1.2,'chave3':'novoteste'}
 
-my_dict3 = {'chave5':my_list}
+my_dict3 = {'chave5':my_list,'chave3':{'key1':'ola'}}
 
 
 print(my_dict)
 print(my_dict2)
 print(my_dict3)
+
+# PODEMOS TER UM DICIONARIO DENTRO DE OUTRO DICIONARIO (ANINHA OS DICIONARIOS)
+print(my_dict3['chave3'])
+
+# MÉTODOS
+
+# METODOS (.KEIS) INFORMA TODAS AS CHAVES DENTRO DO SEU DICIONARIO.
+print(my_dict.keys())
+
+
+
+
