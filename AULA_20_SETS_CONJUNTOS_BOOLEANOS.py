15b2f2a9a8bfc65647242334dfff99803132b808
faqueti <afaqueti@gmail.com>
2019-01-05 10:58:13 -0200

    SETS E BOOLEANOS

--- AULA_20_SETS_CONJUNTOS_BOOLEANOS.py
+++ AULA_20_SETS_CONJUNTOS_BOOLEANOS.py
@@ -5,4 +5,10 @@ print(x)
 
 # NÃO CONFUNDIR O RESULTADO COMO CHAVES SENDO UM DICIONARIO.
 x.add(1)
+x.add(2)
 print(x)
+
+# o resultado serve para retornar a lista ordenada, conforme seus valores e não repetindo
+lista = [11,11,11,22,22,22,4,4,5,5,8,8,8,9,10,10]
+setlista = set(lista)
+print(setlista)
