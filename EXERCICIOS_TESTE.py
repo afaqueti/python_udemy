1f4295f4c883ab97f99e8ea3e571477fce38ef98
faqueti <afaqueti@gmail.com>
2019-01-14 23:38:03 -0200

    EXERCICIO - TESTE - AULA - 12-14-16-18-19-20

--- EXERCICIOS_TESTE.py
+++ EXERCICIOS_TESTE.py
@@ -1,4 +1,5 @@
 
+print('\n{:=^80}'.format(' método 01 '))
 s ='hello'
 print(s)
 print('Total de caracteres: ',len(s))
@@ -11,7 +12,7 @@ print('='*80)
 print(s)
 for i in range(1,len(s)+1):
    print('{} - {}'.format(i,s[i-1:i]))
-print('\nA ultima letra é :', s[res:len(s)])
+print('\nA ultima letra é :',s[res:len(s)])
 
 
 ss = []
@@ -19,8 +20,10 @@ print('='*80)
 print(s)
 for i in range(1,len(s)+1):
     ss.append(i)
-print(ss)
-print(ss.pop())
+print('Lista Gerada',ss)
+print('Ultimo número da lista é {}'
+      '\nportanto a ultima letra é {}:'.format(ss.pop(),s[res:len(s)]))
+
 
 
 
