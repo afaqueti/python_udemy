c5d512646940d4d4ba53a5f4c63f0bc67026ae43
faqueti <afaqueti@gmail.com>
2019-01-23 23:00:46 -0200

    AULA_25_OPERADORES

--- AULA_25_OPERADORES_DE_COMPARACAO_EM_CADEIA.py
+++ AULA_25_OPERADORES_DE_COMPARACAO_EM_CADEIA.py
@@ -4,8 +4,10 @@ a = 1
 b = 2
 c = 1
 res1 = a == c
-res2 = a == c and b == 2 and c == 1
-res3 = a == 2 or b == 4
+res2 = a == c and b == 2 and c == 2  # RETORNA VERDADEIRO TODOS DEVE SER VERDADEIROS
+res3 = a == 2 or b == 2  # SENDO SOMENTE UM VERDADEIRO O RETORNO E "TRUE"
+res4 = (a != b or a == c) and b == 2
 print(res1)
 print(res2)
 print(res3)
+print(res4)
