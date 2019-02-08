be01b7cb98ff1e5511f9242f084fe20a80b77758
faqueti <afaqueti@gmail.com>
2019-02-07 23:54:14 -0200

    AULA 30 RANGE

---
+++ AULA_30_RANGE.py
@@ -0,0 +1,17 @@
+# range
+
+i = range(1,10 +1)
+p = list(i)
+print(p)
+print(i)
+
+i = range(0,10,2)
+p = list(i)
+print(p)
+print(i)
+
+for i in range(1,1000,1):
+    print(i)
+    if i >= 10:
+        break
+
