c01d775470b9c4508d596ea8039937fe62784887
faqueti <afaqueti@gmail.com>
2019-02-07 23:19:10 -0200

    AULA 29 WHILE

--- AULA_29_WHILE.py
+++ AULA_29_WHILE.py
@@ -8,3 +8,15 @@ while x < 10 and y < 20:
     y += 2
 else:
     print('valor de x - y é:' ,x*y)
+
+
+# BREAK
+
+x = 1
+lista = []
+while True:
+    lista += [x]
+    x += 1
+    if x > 10:
+        break
+        print(lista)
