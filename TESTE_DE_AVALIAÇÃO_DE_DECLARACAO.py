942d5e7878ed6a89656b3f53814ee79302579856
faqueti <afaqueti@gmail.com>
2019-02-08 22:53:20 -0200

    TESTE DE AVALIAÇÃO DE DECLARACOES

--- TESTE_DE_AVALIAÇÃO_DE_DECLARACAO.py
+++ TESTE_DE_AVALIAÇÃO_DE_DECLARACAO.py
@@ -13,4 +13,24 @@ for i in s:
         print(i)
 
 
+for i in st.split():
+    if i[0] != 's':
+        print(i)
+
+
+#===================================================================================================
+
+'''
+USE RANGE() PARA IPRIMIR TODOS OS NÚMEROS PARES DE 0 A 10
+'''
+
+for i in range(0,11,2):
+    print(i,end=' ')
+
+#===================================================================================================
+
+'''
+USE A COMPREENSÃO DE LISTA PARA CRIAR UMA LISTA DE TODOS OS NÚMEROS ENTRE 1 A 50 QUE SÃO DIVISIVEIS POR 3
+'''
+
 
