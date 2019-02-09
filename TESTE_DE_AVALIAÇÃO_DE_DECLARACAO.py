52ce257f52c057c49f083b9cf2120c150ab965cb
faqueti <afaqueti@gmail.com>
2019-02-08 22:41:40 -0200

    TESTE DE AVALIAÇÃO DE DECLARACOES

--- TESTE_DE_AVALIAÇÃO_DE_DECLARACAO.py
+++ TESTE_DE_AVALIAÇÃO_DE_DECLARACAO.py
@@ -5,3 +5,12 @@
 USE FOR, SPLIT() E IF PARA DECLARAÇÃO QUE IMPRIMA AS PALAVRAS QUE COMEÇAM COM 'S'
 
 '''
+
+st = 'Print only the words that start with s in this sentence'
+s = st.split()
+for i in s:
+    if i[0] == 's':
+        print(i)
+
+
+
