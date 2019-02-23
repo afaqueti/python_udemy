4805442b4674a7b3694a36aa13ef0d4ffae97109
faqueti <afaqueti@gmail.com>
2019-02-22 23:44:56 -0300

    Teste de nova função

--- AULA_FUNCOES.py
+++ AULA_FUNCOES.py
@@ -1,9 +1,9 @@
 # FUNÇÕES
 
 def primeira_funcao():
-    '''
+    """
     ola mundo
-    '''
+    """
     st = 'Print only the word that start with s in this sentence'
     s = st.split()
     for i in s:
@@ -11,12 +11,32 @@ def primeira_funcao():
         if ta % 2 == 0:
             print('A palavra ({}) é par'.format(i))
 
+
 primeira_funcao()
 
-print('='*80)
+print('=' * 80)
+
 
 def segunda_funcao():
+    """""
+    Teste para nome de funções
+    """""
     n1 = 'teste'
     print(n1)
 
+
 segunda_funcao()
+
+print('=' * 80)
+
+
+def somar(num1, num2):
+    print(num1 + num2)
+somar(2, 5)
+
+print('=' * 80)
+
+
+def subtrair(n1, n2):
+    return n1 - n2
+print(subtrair(10, 2))
