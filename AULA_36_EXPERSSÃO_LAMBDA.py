5ae28b1bb21ba01b44e3c10d0caf70faf34a77f2
faqueti <afaqueti@gmail.com>
2019-03-04 10:17:28 -0300

    Teste de nova função

---
+++ AULA_36_EXPERSSÃO_LAMBDA.py
@@ -0,0 +1,39 @@
+# EXPERSSÃO LAMBDA
+
+def square(num):
+    res = num ** 2
+    return res
+print(square(16))
+
+
+print('='*80)
+
+# RETORNO OTMIZADO
+
+def square_2(num):
+    return num ** 2
+print(square_2(4))
+
+print('='*80)
+
+def square_3(num): return num ** 2
+print(square_3(12))
+
+def par(x):
+    res = x % 2 == 0
+    return res
+if par(3) == True:
+    print('par')
+else:
+    print('impar')
+
+
+# A FUNÇÃO "LAMBDA" É UM METODO E CRIAÇÃO DE FUNÇÃO OTMIZADA.
+
+x = lambda num: num ** 2
+print(x(10))
+
+
+par = lambda x: x % 2 == 0
+print(par(2))
+
